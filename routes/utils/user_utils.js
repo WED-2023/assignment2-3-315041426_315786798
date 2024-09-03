d16a88fefded227934c43a0566f28e722a06927e
GilMich <gilmich@post.bgu.ac.il>
2024-09-03 19:03:22 +0300

    getting the user favorites recipes now also works

--- routes/utils/user_utils.js
+++ routes/utils/user_utils.js
@@ -5,12 +5,12 @@ async function markAsFavorite(userID, recipeID){
 }
 
 async function getFavoriteRecipes(user_id){
-    const recipes_id = await DButils.execQuery(`select recipe_id from FavoriteRecipes where user_id='${user_id}'`);
+    const recipes_id = await DButils.execQuery(`select recipeID from FavoriteRecipes where userID='${user_id}'`);
     return recipes_id;
 }
 
 async function getMyRecipes(userID){
-    const my_recipes = await DButils.execQuery(`select recipe_id,recipeName,ingredients,instructions,vegan,glutenFree from my-recipes where user_id='${userID}'`);
+    const my_recipes = await DButils.execQuery(`select recipeID,recipeName,ingredients,instructions,vegan,glutenFree from my-recipes where userID='${userID}'`);
     return my_recipes;
 
 
